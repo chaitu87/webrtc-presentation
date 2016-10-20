884c208122af4670e53cf0b0c2efd1fd45601476
Chaithanya <chaithanya@theinnerhour.com>
2016-10-21 03:38:51 +0530

    listening presentation.. pretty cool huh

---
+++ public/js/app.js
@@ -0,0 +1,65 @@
+window.pubkey = "pub-c-f424178f-364d-4033-a51b-0ff82e5814e5";
+window.subkey = "sub-c-3c46a32e-970e-11e6-bb35-0619f8945a4f";
+
+window.pubnub = new PubNub({
+    subscribeKey: window.subkey,
+    publishKey: window.pubkey,
+    ssl: true
+})
+
+$(function() {
+    console.log("this is jquery");
+    // Set the slide height
+    var bodyheight = $('body').outerHeight() - 24;
+    var bodywidth = $('body').outerWidth();
+    console.log(bodyheight);
+    console.log(bodywidth);
+    $('.item').css('height', bodyheight + 'px').css('width', bodywidth + 'px');
+});
+
+$(function() {
+    var slides = $('.item');
+    var maxslides = slides.length;
+    var startslide = 0;
+    $.each(slides, function(index, slide) {
+        $(slide).attr('data-hash', index);
+    });
+    var presenter = $('.owl-carousel').owlCarousel({
+        URLhashListener: true,
+        items: 1,
+        nav: true,
+        center: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    // Subscribe to slide change events
+    presenter.on('changed.owl.carousel', $.debounce(300, function(e) {
+        window.pubnub.publish({
+            channel: 'tro-presentation',
+            message: {
+                goto: e.item.index
+            }
+        });
+    }));
+
+    // Listening to pubnub channels
+    window.pubnub.addListener({
+        message: function(m) {
+        	console.log('got a change slide event');
+            presenter.trigger('to.owl.carousel', m.message.goto);
+        }
+    });
+    window.pubnub.subscribe({
+        channels: ['tro-presentation']
+    })
+});
